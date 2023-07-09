675ff66528f77d63e5278813ecaeb06b2513a68b
“abiodun603” <abioduncsc@gmail.com>
2023-07-09 14:46:26 +0100

    remove default

--- src/app/auth/component/Signin.tsx
+++ src/app/auth/component/Signin.tsx
@@ -27,13 +27,18 @@ interface UserData {
   password: string
 }
 
+// const defaultValues = {
+//   email: 'ab3sure@gmail.com',
+//   password: 'P@ssword88283'
+// }
+
 const defaultValues = {
-  email: 'ab3sure@gmail.com',
-  password: 'P@ssword88283'
+  email: '',
+  password: ''
 }
 
 const Signin = () => {
-  const methods = useForm();
+  const methods = useForm({defaultValues});
 
   // ** Hooks
   const auth = useAuth()
